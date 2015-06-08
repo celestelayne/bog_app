d352f07cbc17a64876fa0ac6c756431c24a1cbb8
Celeste Layne <layne.celeste@gmail.com>
2015-06-08 09:26:59 -0700

    first commit

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,50 @@
+class CreaturesController < ApplicationController
+	
+	def index
+		@creatures = Creature.all
+		render :index
+	end
+
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	def show
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :show
+	end
+
+	def create
+		new_creature = params.require(:creature).permit(:name, :description)
+		creature.create(new_creature)
+		redirect_to '/creatures'
+	end
+
+	def edit
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
+	end
+
+	def update
+		creature_id = params[:id]
+		creature = Creature.find(Creature_id)
+
+		updated_attributes = params.require(:creature).permit(:name, :description)
+
+		creature.updated_attributes(updated_attributes)
+
+		redirect_to '/creatures/#{creature_id}'
+
+	end
+
+	def destroy
+		id = params[:id]
+		creature = Creature.find(id)
+		creature.destroy
+		redirect_to '/creatures'		
+	end
+
+end
